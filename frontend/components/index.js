52ccc19e1fb2aab3f65cdbb650a779dd0ae4ac54
Joe <jguss45@gmail.com>
2023-06-21 12:05:30 +0200

    create project outline

---
+++ frontend/components/index.js
@@ -0,0 +1,11 @@
+import LocationMenu from "./LocationMenu";
+import Map from "./Map";
+import Navbar from "./Navbar";
+import Suggestions from "./Suggestions";
+
+export {
+    LocationMenu,
+    Map,
+    Navbar,
+    Suggestions
+}
