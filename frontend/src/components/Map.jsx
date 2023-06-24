584ed034a24e0e295f1e99179e6d4e4e4c37853c
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-06-24 17:47:17 +0200

    advancements

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import L from "leaflet"; // Import Leaflet library
 
 function Map() {
+  /*
   useEffect(() => {
     var map = L.map("map").setView([51.505, -0.09], 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -10,6 +11,7 @@ function Map() {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
   }, []); // Empty dependency array to run the effect only once
+  */
 
   return <div style={{ height: "180px" }} id="map"></div>;
 }
