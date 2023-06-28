b101686e1b8901edc63bb5ea98f598e1d0334d78
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-06-28 15:59:14 +0200

    advancements

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,54 @@
 import React from "react";
 import Footer from "../components/Footer";
 
+
+
 const Home = () => {
   return (
     <>
-      <h1 class="mb-5 text-6xl font-bold leading-none tracking-normal text-purple-950 md:text-5xl lg:text-6xl dark:text-white text-center pr-3 pl-3 ps-px">
+      <h1 className="mb-5 text-6xl font-bold leading-none tracking-normal text-purple-950 md:text-5xl lg:text-6xl dark:text-white text-center pr-3 pl-3 ps-px">
         Innovating solar panels allocation with{" "}
-        <span class="text-pink-500 dark:text-pink-600">machine learning</span>{" "}
+        <span className="text-pink-500 dark:text-pink-600">
+          machine learning
+        </span>{" "}
       </h1>
-      <p class="text-lg font-normal text-gray-600 lg:text-xl dark:text-gray-400 text-center pr-3 pl-3">
-        Machine Learning model to optimize the research of optimal solar panels 
+      <p className="text-lg font-normal text-gray-600 lg:text-xl dark:text-gray-400 text-center pr-3 pl-3">
+        Machine Learning model to optimize the research of optimal solar panels
         location for Barter
       </p>
+
+      <div className="bg-white p-5 rounded-lg mt-5 shadow-md">
+        <p className="text-sm font-normal text-black lg:text-base text-center pr-3 pl-3">
+          Welcome to Barter At Barter, we're committed to revolutionizing the
+          way we use and distribute energy. We facilitate ultra-fast charging
+          for electric vehicles using solar energy, democratize solar energy
+          without a roof and without investment, and monetize roofs by creating
+          solar communities.
+        </p>
+      </div>
+
+      <div className="bg-white p-5 rounded-lg mt-5 shadow-md">
+        <p className="text-sm font-normal text-black lg:text-base text-center pr-3 pl-3">
+          Our Project Our main goal is to develop a system for identifying
+          optimal locations for new facilities based on electricity consumption
+          data linked to cadastral references. We're integrating SIPS (Supply
+          Point Information System) data with cadastral data of a geographic
+          area and using our existing internal tool to estimate monthly/annual
+          electricity consumption data. This will enrich our dataset with
+          consumption information, allowing us to make informed decisions on the
+          location of future installations.
+        </p>
+      </div>
+
+      <div className="bg-white p-5 rounded-lg mt-5 shadow-md">
+        <p className="text-sm font-normal text-black lg:text-base text-center pr-3 pl-3">
+          Join Our Energy Revolution Whether you're a business looking to
+          conserve cash and gain new customers, or an individual interested in
+          sustainable energy solutions, we invite you to join us. Together, we
+          can create a future where energy is more accessible, efficient, and
+          environmentally friendly.
+        </p>
+      </div>
 
       <Footer />
     </>
--- frontend/src/pages/Model.jsx
+++ frontend/src/pages/Model.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import Footer from '../components/Footer';
-
-
+import React from "react";
+import Footer from "../components/Footer";
 
 const Model = () => {
   return (
     <>
       <h1 className="mb-5 text-6xl font-bold leading-none tracking-normal text-purple-950 md:text-5xl lg:text-6xl dark:text-white text-center pr-3 pl-3 ps-px">
-        The model has been developed using{" "}
-        <span className="text-pink-500 dark:text-pink-600">
-          machine learning ????
-        </span>{" "}
+        Machine Learning at the hearth of{" "}
+        <span className="text-pink-500 dark:text-pink-600">innovation</span> and{" "}
+        <span className="text-pink-500 dark:text-pink-600">efficiency</span>{" "}
       </h1>
       <p className="text-lg font-normal text-gray-600 lg:text-xl dark:text-gray-400 text-center pr-3 pl-3">
-        BLA BLA BLA BLA BLA BLA
+        This machine learning model developed by the team aims at increasing the efficiency of the allocation of solar panels in Spain
       </p>
 
-      <h3 class="text-3xl font-bold text-purple-950 dark:text-white">
-        Technologies
+      <h3 class="text-center pt-5 pb-2 text-3xl font-bold text-purple-950 dark:text-white">
+        Technologies used
       </h3>
       <span class="inline-grid grid-cols-3 gap-4">
         <span>TECH 1</span>
@@ -36,19 +33,40 @@ const Model = () => {
         <span>IMAGE 6</span>
       </span>
 
-      <h3 class="text-3xl font-bold text-purple-950 dark:text-white">
+      <h3 class="text-center pb-2 text-3xl font-bold text-purple-950 dark:text-white">
         Architecture
       </h3>
-      <img src="" alt="" />
 
-      <h3 class="text-3xl font-bold text-purple-950 dark:text-white">
+      <figure class="mx-auto max-w-3xl">
+        <img
+          className="h-auto max-w-full rounded-lg"
+          src="Architecture.jpeg"
+          alt="Model Architecture"
+        />
+        <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
+          Architecture of the Project
+        </figcaption>
+      </figure>
+
+      <h3 class="pt-5 text-center pb-2 text-3xl font-bold text-purple-950 dark:text-white">
         The Jupyter Notebook
       </h3>
-      <iframe src="geospatial-model.html" width="100%" height="800px"></iframe>
-
+      <div className="w-4/5 mx-auto h-96">
+        <div className="border border-purple-950 rounded p-6 h-full">
+          <iframe
+            className="w-full h-full"
+            src="geospatial-model.html"
+            title="Machine Learning Model Notebook"
+          ></iframe>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
       <Footer />
     </>
   );
-}
+};
 
-export default Model
+export default Model;
