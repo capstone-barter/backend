04c96e15ee710b3e15cbd66631af98c21a5cd209
Joe <jguss45@gmail.com>
2023-06-29 00:40:45 +0200

    changed map zoom to malaga

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -20,8 +20,8 @@ const Map = () => {
       container: 'map', // container ID
       // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-3.7038, 40.4168], // starting position [lng, lat]
-      zoom: 9 // starting zoom
+      center: [-4.4214, 36.7213], // starting position [lng, lat]
+      zoom: 12 // starting zoom
     });
 
     // Cleanup map instance on component unmount
