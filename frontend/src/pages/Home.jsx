c518a4da8655556682b8f8a7ba8cdcebc447aa00
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-06-30 11:31:45 +0200

    advancements

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,10 +13,10 @@ const Home = () => {
         </span>{" "}
       </h1>
       <p className="pb-7 text-lg font-normal text-gray-600 lg:text-xl dark:text-gray-400 text-center pr-3 pl-3">
-        Machine Learning model to optimize the research of optimal solar panels
-        location for Barter
+        The team developed a machine learning model using the latest
+        technologies available to find the most suitable location to install
+        solar panels
       </p>
-      <img class="h-auto max-w-full" src="barter_image.jpg" alt="image barter" />
 
       <div className="bg-white p-5 rounded-lg mt-5 shadow-md">
         <p className="text-sm font-normal text-black lg:text-base text-center pr-3 pl-3">
@@ -51,6 +51,19 @@ const Home = () => {
         </p>
       </div>
 
+      <div className="relative">
+        <img
+          className="h-auto max-w-full"
+          src="barter_image.jpg"
+          alt="image barter"
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex items-center pl-6">
+          <h1 className="text-5xl text-white font-bold">#InnovatingWithAI</h1>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
       <Footer />
     </>
   );
