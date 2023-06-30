236d868fbb620e5a8fb19967dda061172129ea10
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-06-30 16:46:06 +0200

    advancements

--- frontend/src/pages/Locator.jsx
+++ frontend/src/pages/Locator.jsx
@@ -7,8 +7,11 @@ const Locator = () => {
   return (
     <>
       <h1>Text Placeholder</h1>
-      <Map />
-      <ZipCodeConverter /> 
+      <div className="w-3/4 p-4  p-5 rounded-lg mt-5 text-md h-96">
+        <Map />
+      </div>
+      
+      <ZipCodeConverter />
     </>
   );
 };
