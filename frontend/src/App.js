606e84134fd3fbe4186a00848d87f42231515b5c
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-07-03 17:54:22 +0200

    advancements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
+import Login from "./components/Login";
 import NavBar from "./components/Navbar";
+import useToken from "./components/useToken";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Locator from "./pages/Locator";
 import Model from "./pages/Model";
-
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const { token, setToken } = useToken();
 
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
     <div className="App">
-        <NavBar />
-        <div className="mt-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/Locator" element={<Locator />} />
-            <Route path="/Model" element={<Model />} />
-          </Routes>
-        </div>
-        
+      <NavBar />
+      <div className="mt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Locator" element={<Locator />} />
+          <Route path="/Model" element={<Model />} />
+        </Routes>
+      </div>
     </div>
   );
 }
