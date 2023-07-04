43f740a8e94b56f91d7d12cbcb6cad2c0aa5b8ac
Joe <jguss45@gmail.com>
2023-07-04 17:44:05 +0200

    removed unused button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,12 +21,6 @@ function NavBar() {
           </Link>
           <div className="flex md:order-2">
             <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
-            >
-              Malaga Results
-            </button>
-            <button
               data-collapse-toggle="navbar-sticky"
               type="button"
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
